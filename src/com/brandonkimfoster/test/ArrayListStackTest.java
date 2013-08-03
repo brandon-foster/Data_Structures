4d890af7f527d268e1bcb34ae3b02e338b763034
Brandon-Foster <brandon.foster.vt@gmail.com>
2013-08-03 19:24:21 -0400

    Write tests for ArrayListStack.push()

--- src/com/brandonkimfoster/test/ArrayListStackTest.java
+++ src/com/brandonkimfoster/test/ArrayListStackTest.java
@@ -5,7 +5,11 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.brandonkimfoster.concrete.ArrayListStack;
+
 public class ArrayListStackTest {
+	
+	ArrayListStack<String> stack;
 
 	/**
 	 * Perform setup actions common to all test cases.
@@ -14,6 +18,8 @@ public class ArrayListStackTest {
 	 */
 	@Before
 	public void setUp() throws Exception {
+		
+		stack = new ArrayListStack<String>();
 	}
 
 	/**
@@ -21,16 +27,31 @@ public class ArrayListStackTest {
 	 */
 	@Test
 	public void testPush() {
-		fail("Not yet implemented");
-	}
-	
-	/**
-	 * Check that the pop() method removes and returns the item at the top of
-	 *  the stack.
-	 */
-	@Test
-	public void testPop() {
-		fail("Not yet implemented");
+		
+		// pushes a valid item on the stack
+		stack.push("string1");
+		
+		assertEquals(1, stack.size());
+		assertEquals("string1", stack.peek());
+		
+		stack.push("string2");
+		
+		assertEquals(2, stack.size());
+		assertEquals("string2", stack.peek());
+		
+		// pushes a null item on the stack
+		String nullItem = null;
+		
+		Exception thrown = null;
+		try {
+			stack.push(nullItem);
+		}
+		catch (Exception exception) {
+			thrown = exception;
+		}
+		
+		assertNotNull(thrown);
+		assertTrue(thrown instanceof NullPointerException);
 	}
 
 }

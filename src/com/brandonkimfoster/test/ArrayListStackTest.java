1eee30920dfd22b46d6b06c62e981a393da1a498
Brandon-Foster <brandon.foster.vt@gmail.com>
2013-08-03 15:59:16 -0400

    Create ArrayListStackTest and stub test cases for push() and pop()

---
+++ src/com/brandonkimfoster/test/ArrayListStackTest.java
@@ -0,0 +1,36 @@
+package com.brandonkimfoster.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ArrayListStackTest {
+
+	/**
+	 * Perform setup actions common to all test cases.
+	 * 
+	 * @throws Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Check that the push() method adds an item at the top of the stack.
+	 */
+	@Test
+	public void testPush() {
+		fail("Not yet implemented");
+	}
+	
+	/**
+	 * Check that the pop() method removes and returns the item at the top of
+	 *  the stack.
+	 */
+	@Test
+	public void testPop() {
+		fail("Not yet implemented");
+	}
+
+}

a88a39bb41942d473893cb061d34211c00d17fa8
Brandon-Foster <brandon.foster.vt@gmail.com>
2013-08-03 19:45:27 -0400

    Move Javadoc for ArrayListStack's constructor to the appropriate place Modify Javadoc for ArrayListStackTest.testPush() Write test for ArrayListStack.pop()

--- src/com/brandonkimfoster/test/ArrayListStackTest.java
+++ src/com/brandonkimfoster/test/ArrayListStackTest.java
@@ -2,6 +2,8 @@ package com.brandonkimfoster.test;
 
 import static org.junit.Assert.*;
 
+import java.util.EmptyStackException;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -23,28 +25,29 @@ public class ArrayListStackTest {
 	}
 
 	/**
-	 * Check that the push() method adds an item at the top of the stack.
+	 * Check that the push() method adds an item on the top of the stack and
+	 * throws a NullPointerException if a null item is pushed on the stack.
 	 */
 	@Test
 	public void testPush() {
 		
 		// pushes a valid item on the stack
-		stack.push("string1");
+		this.stack.push("string1");
 		
-		assertEquals(1, stack.size());
-		assertEquals("string1", stack.peek());
+		assertEquals(1, this.stack.size());
+		assertEquals("string1", this.stack.peek());
 		
-		stack.push("string2");
+		this.stack.push("string2");
 		
-		assertEquals(2, stack.size());
-		assertEquals("string2", stack.peek());
+		assertEquals(2, this.stack.size());
+		assertEquals("string2", this.stack.peek());
 		
 		// pushes a null item on the stack
 		String nullItem = null;
 		
 		Exception thrown = null;
 		try {
-			stack.push(nullItem);
+			this.stack.push(nullItem);
 		}
 		catch (Exception exception) {
 			thrown = exception;
@@ -52,6 +55,42 @@ public class ArrayListStackTest {
 		
 		assertNotNull(thrown);
 		assertTrue(thrown instanceof NullPointerException);
+	}
+	
+	/**
+	 * Check that the pop() method removes the item on the top of the stack and
+	 * throws an EmptyStackException if pop() is called on an empty stack.
+	 */
+	@Test
+	public void testPop() {
+		
+		int numItems = 5;
+		
+		// push five items on the stack
+		for (int i = 0; i < numItems; i++) {
+			this.stack.push("#" + i);
+		}
+		
+		// pop all items off the stack
+		for (int i = 0; i < numItems; i ++) {
+			this.stack.pop();
+			int expectedStackSize = numItems - (i + 1);
+			assertEquals(expectedStackSize, this.stack.size());
+		}
+		
+		// pop an empty stack
+		
+		Exception thrown = null;
+		try {
+			this.stack.pop();
+		}
+		catch (Exception exception) {
+			thrown = exception;
+		}
+		System.out.println(thrown);
+		System.out.println(this.stack.size());
+		assertNotNull(thrown);
+		assertTrue(thrown instanceof EmptyStackException);
 	}
 
 }

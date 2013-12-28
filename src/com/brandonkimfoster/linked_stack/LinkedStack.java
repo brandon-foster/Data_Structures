bfbe392a87b819087afc2381f07327465e0de32d
Brandon-Foster <brandon.foster.vt@gmail.com>
2013-12-28 18:51:05 -0500

    Write LinkedStack

---
+++ src/com/brandonkimfoster/linked_stack/LinkedStack.java
@@ -0,0 +1,70 @@
+package com.brandonkimfoster.linked_stack;
+
+import java.util.EmptyStackException;
+
+import com.brandonkimfoster.array_list_stack.Stack;
+import com.brandonkimfoster.linked_stack.Node;
+
+public class LinkedStack<T> implements Stack<T> {
+	
+	private Node<T> top;
+	private int size;
+	
+	public LinkedStack() {
+		this.top = null;	
+		this.size = 0;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void push(T item) {
+		if (item == null) {
+			throw new NullPointerException();
+		}
+		Node<T> newNode = new Node<T>(item);
+		newNode.setPrevious(this.top);
+		this.top = newNode;
+		this.size++;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public T pop() throws EmptyStackException {
+		if (this.isEmpty()) {
+			throw new EmptyStackException();
+		}
+		T theData = top.data();
+		this.top = top.previous();
+		this.size--;
+		return theData;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public T peek() {
+		
+		return this.top.data();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public int size() {
+		return this.size;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public boolean isEmpty() {
+		return (this.size == 0) ? true : false;
+	}
+}

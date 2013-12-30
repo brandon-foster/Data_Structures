7eee87ac4a7eef75af70cfa6d9020e09800b68cf
Brandon-Foster <brandon.foster.vt@gmail.com>
2013-12-30 13:41:02 -0500

    Modify fields, methods, and comments to make a Node more general

--- src/com/brandonkimfoster/Node.java
+++ src/com/brandonkimfoster/Node.java
@@ -1,60 +1,53 @@
 package com.brandonkimfoster;
 
 /**
- * A node stores a data item and a reference to the node that came before it.
- * If there is no node that precedes the reference contains a null value.
+ * A node wraps a data item and a reference (called a "link" here) to another
+ * node. If there is no other node to be linked to, the reference is null.
  * 
  * @author Brandon Foster
  */
-public class Node<T>
-{
+public class Node<T> {
 	// The item the node holds
-    private T data;
-    
-    // Reference to the previous node
-    private Node<T> previous;
+	private T data;
 
+	// Reference to another node
+	private Node<T> link;
 
-    // Constructor
-    /**
-     * Creates a new Node with the passed in data.
-     *
-     * @param data the data for the node
-     */
-    public Node(T data)
-    {
-        this.data = data;
-        this.previous = null;
-    }
+	// Constructor
+	/**
+	 * Creates a new Node with the passed in data
+	 * 
+	 * @param data
+	 *            the data for the node
+	 */
+	public Node(T data) {
+		this.data = data;
+		this.link = null;
+	}
 
-    // ----------------------------------------------------------
-    /**
-     * Gets the data in the node.
-     *
-     * @return the data in the node
-     */
-    public T data()
-    {
-        return this.data;
-    }
+	/**
+	 * Gets the data in the node.
+	 * 
+	 * @return the data in the node
+	 */
+	public T data() {
+		return this.data;
+	}
 
-    // ----------------------------------------------------------
-    /**
-     * Gets the node that precedes this one in the sequence.
-     *
-     * @return the node that precedes this one in the sequence
-     */
-    public Node<T> previous()
-    {
-        return this.previous;
-    }
-    
-    /**
-     * Sets the reference to the previous node
-     */
-    public void setPrevious(Node<T> node)
-    {
-    	this.previous = node;
-    }
-    
+	/**
+	 * Gets the node that this node links to
+	 * 
+	 * @return the node that is linked to by this node
+	 */
+	public Node<T> getLink() {
+		return this.link;
+	}
+
+	/**
+	 * Sets the node's reference to link to the specified node
+	 */
+	public void setLink(Node<T> node) {
+		this.link = node;
+	}
+
 }
